d1a527dfa716e60aa6d0dcb7a3a30de661eb68da
albert <1132265836@qq.com>
2022-01-12 11:46:27 +0800

    git 第二次修改

--- src/main/java/com/ccy/git/MainApplication.java
+++ src/main/java/com/ccy/git/MainApplication.java
@@ -3,5 +3,6 @@ package com.ccy.git;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("第一次修改");
+        System.out.println("第二次修改");
     }
 }
